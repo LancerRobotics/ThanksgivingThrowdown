86216e5481e65ff209c1ca43cc7c73ada809c503
alm0st <alan.roy43@outlook.com>
2024-08-07 10:49:48 -0400

    Add bot constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lancers/LancersBotConfig.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.lancers;
+
+public class LancersBotConfig {
+    public final static String WEBCAM = "webcam";
+    public final static String FRONT_LEFT_MOTOR = "front_left_motor";
+    public final static String BOTTOM_LEFT_MOTOR = "bottom_left_motor";
+    public final static String BOTTOM_RIGHT_MOTOR = "bottom_right_motor";
+    public final static String FRONT_RIGHT_MOTOR = "front_right_motor";
+    public final static String CLAW_SERVO = "claw_servo";
+}
