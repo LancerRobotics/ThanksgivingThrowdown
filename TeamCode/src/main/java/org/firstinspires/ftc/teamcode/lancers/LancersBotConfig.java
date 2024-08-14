f161d304d6cd88bf4459f9188d124fb3eb1b36d7
alm0st <alan.roy43@outlook.com>
2024-08-14 15:22:47 -0400

    Fix slides motor and claw servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lancers/LancersBotConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lancers/LancersBotConfig.java
@@ -6,6 +6,6 @@ public class LancersBotConfig {
     public final static String BOTTOM_LEFT_MOTOR = "bottom_left_motor";
     public final static String BOTTOM_RIGHT_MOTOR = "bottom_right_motor";
     public final static String FRONT_RIGHT_MOTOR = "front_right_motor";
-    //public final static String CLAW_SERVO = "claw_servo";
+    public final static String CLAW_SERVO = "claw_servo";
     public final static String SLIDES_MOTOR = "claw_motor";
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lancers/LancersTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lancers/LancersTeleOp.java
@@ -2,7 +2,8 @@ package org.firstinspires.ftc.teamcode.lancers;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.*;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp()
 public class LancersTeleOp extends LinearOpMode {
@@ -22,15 +23,15 @@ public class LancersTeleOp extends LinearOpMode {
             final DcMotor rightRear = hardwareMap.dcMotor.get(LancersBotConfig.BOTTOM_RIGHT_MOTOR);
 
             final DcMotor slidesMotor = hardwareMap.dcMotor.get(LancersBotConfig.SLIDES_MOTOR);
-            //final DcMotor clawServo = hardwareMap.dcMotor.get(LancersBotConfig.CLAW_SERVO);
+            final Servo clawServo = hardwareMap.servo.get(LancersBotConfig.CLAW_SERVO);
 
             // Gamepad positions; Motors are swapped
             final double ly = -gamepad1.left_stick_y * speedMultiplier; // Remember, Y stick value is reversed
             final double lx = gamepad1.left_stick_x * speedMultiplier; // Counteract imperfect strafing
             final double rx = gamepad1.right_stick_x * speedMultiplier;
 
-            final double spy = gamepad2.left_trigger * speedMultiplier;
-            final double sny = -gamepad2.right_trigger * speedMultiplier;
+            final double spy = gamepad2.left_trigger;
+            final double sny = gamepad2.right_trigger;
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
@@ -42,7 +43,34 @@ public class LancersTeleOp extends LinearOpMode {
             final double frontRightPower = (ly - lx - rx) / denominator;
             final double backRightPower = (ly + lx - rx) / denominator;
 
-            final double slidesPower = spy+sny;
+            double slidesPower = 0.0d;
+            final float TRIGGER_THRESHOLD = 0.15f;
+
+            if (spy > TRIGGER_THRESHOLD){
+                //telemetry.addData("SPY USED!!!", spy);
+                slidesPower = spy;
+                slidesPower = (spy - TRIGGER_THRESHOLD) * (1f / (1f - TRIGGER_THRESHOLD));
+            }
+            if (sny > TRIGGER_THRESHOLD){
+                //telemetry.addData("SNY USED!!!", sny);
+                slidesPower =  -(sny - TRIGGER_THRESHOLD) * (1f / (1f - TRIGGER_THRESHOLD));
+            }
+
+            double currentServoPosition = clawServo.getPosition();
+            final double openServoPosition = 0;
+            final double closeServoPosition = 0.5;
+            final double servoSpeed = 0.01;
+
+            if ((gamepad2.a) && currentServoPosition < closeServoPosition){
+                currentServoPosition += servoSpeed;
+            }
+            else if ((gamepad2.b) && currentServoPosition > openServoPosition){
+                currentServoPosition -= servoSpeed;
+            }
+
+            currentServoPosition = Range.clip(currentServoPosition, openServoPosition, closeServoPosition);
+
+            clawServo.setPosition(currentServoPosition);
 
             leftFront.setPower(-frontLeftPower);
             leftRear.setPower(-backLeftPower);
